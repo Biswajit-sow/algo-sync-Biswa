0e53b29c225a5b9d251cc25e48d788c79987b363
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2025-12-28 10:13:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (88.48%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,15 +1,35 @@
-from typing import List
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        n=len(nums)
+        i=0
+        j=1
+        unique=1
+        while(j<n):
+            if nums[j]==nums[j-1]:
+                j+=1
+                continue
+                
+            
+            nums[i+1]=nums[j]
+            i+=1
+            unique+=1
+            j+=1
+        return unique
 
-        i = 0  # index of last unique element
 
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                i += 1
-                nums[i] = nums[j]
 
-        return i + 1
+'''
+
+def removeDuplicates(self, nums: List[int]) -> int:
+    if not nums: return 0     # (Safety check for empty list)
+    
+    i = 0                     # Line A: The "Keeper" pointer
+    
+    for j in range(1, len(nums)):  # Line B: The "Scout" loop
+        if nums[j] != nums[i]:     # Line C: The "New Number" check
+            i += 1                 # Line D: Move Keeper forward
+            nums[i] = nums[j]      # Line E: Save the new number
+            
+    return i + 1              # Line F: Return the count
+'''
+        
