6d7d3ab63ff2c5f3c774608dea59ae47fc45c22e
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2025-12-31 11:14:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.60%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,7 +1,24 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        arr.sort()
         
+        freq={}
+        for i in  arr:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+        #freq={1: 2, 2: 3}
+        freq_counts=freq.values() #freq_counts={2,3}
+        if len(set(freq_counts))==len(freq_counts):
+            return True
+        return False
+
+
+
+
+
+
+'''
         count=1
         store=[]
         n=len(arr)
@@ -18,6 +35,6 @@ class Solution:
             if store[j]==store[j-1]:
                 return False
         return True
-
+'''
         
         
