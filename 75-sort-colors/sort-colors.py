a8c42e35e7dab32dec5af5f03e319ee02dd7129c
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-01-10 10:12:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.25%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -17,16 +17,16 @@ class Solution:
                 
             elif i==2:
                 two+=1
-         # Second pass: modify nums in-place
+         # Second pass:1st way
         sorted_arr = [0]*zero + [1]*one + [2]*two
-
+         # Second pass: 2nd way
         '''for i in range(zero):
             sorted_arr.append(0)
         for j in range(one):
             sorted_arr.append(1)
         for k in range(two):
             sorted_arr.append(2)'''
-        
+         #modify nums in-place
         for i in range(len(nums)):
             nums[i] = sorted_arr[i]
 
