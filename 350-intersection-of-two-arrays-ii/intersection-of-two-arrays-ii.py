b18c77f651314961f41f50e043656fd70b419cf4
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-01-14 11:40:49 +0530

    Time: 1 ms (51.77%) | Memory: 19.4 MB (8.17%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        output=[]
         n1=len(nums1)
         n2=len(nums2)
-        
-        for i in nums1:
-            if i in nums2:
-                output.append(i)
-                nums2.remove(i) 
+        nums1.sort()
+        nums2.sort()
+        output=[]
+        i=0
+        j=0
+        while i<n1 and j<n2:
+            if nums1[i]==nums2[j]:
+                output.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
             else:
-                
-                continue
+                j+=1
         return output
-
-
-
-
-                
