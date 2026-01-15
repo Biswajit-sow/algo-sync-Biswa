78138e5536b268444348187a976bcb58496c0e25
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-01-15 22:22:02 +0530

    Time: 23 ms (92.16%) | Memory: 31.5 MB (62.87%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -12,7 +12,29 @@ class Solution:
                 
             if current_sum < 0:
                 current_sum = 0
-            
-            # REMOVED line 12: max_sum = max(max_sum, current_sum) 
-            
         return max_sum
+# but in the above code has a bug so I use the below code
+
+'''class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = max_sum = nums[0]  # Initialize properly
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])  # Restart or extend
+            max_sum = max(max_sum, current_sum)
+        return max_sum
+'''
+
+#no bug now
+
+
+
+
+
+
+
+
+
+
+'''https://www.perplexity.ai/search/here-is-a-problem-that-intervi-mrVCjd_QS4G63tMO176h_Q#14
+https://www.perplexity.ai/search/class-solution-def-maxsubarray-8Xqq9485QSiyZ7pIUFa1WA?sm=d#0
+'''
