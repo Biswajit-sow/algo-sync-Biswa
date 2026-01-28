5c0a97838861268ef3fc68a5ddea30a0e2190e19
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-01-28 10:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,3 +1,27 @@
+'''
+brute force 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        len_str=len(s)
+        max_length=0
+        for i in range(len_str):
+            seen=set()
+            count=0
+            for j in range(i,len_str):
+                if s[j] not in seen:
+                    seen.add(s[j])
+                    count+=1
+                else:
+                    break 
+            if count > max_length:
+                max_length=count
+        return max_length
+
+        '''
+
+
+
+'''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         freq={}
@@ -18,6 +42,33 @@ class Solution:
             if max(freq.values())==1:
                 current_len=high-low+1
                 max_len=max(max_len,current_len)
+        return max_len
+
+'''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        low=0
+        freq={}
+        n=len(s)
+        current_len=0
+        max_len= 0
+        
+        for high in range(n):
+            if s[high] in freq:
+                freq[s[high]]+=1
+            else:
+                freq[s[high]]=1
+            k=0
+            k=high-low+1 # window size
+            while(len(freq)<k):
+                freq[s[low]]-=1
+                if(freq[s[low]]==0):
+                    del freq[s[low]]
+                low+=1
+                k=high-low+1
+            # here len(freq)==k, so
+            current_len=high-low+1
+            max_len=max(max_len,current_len)
         return max_len
 
     __import__("atexit").register(
