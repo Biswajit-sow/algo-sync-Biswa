96fee80558ab3b41790b9f2b8297f6a1394825a9
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-01-31 22:29:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (7.65%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -12,12 +12,14 @@ class Solution:
             freq[char_index]+=1
             current_len=high-low+1
             max_count = max(freq)
+            #max_count = max(max_count, freq[char_index])
             diff=current_len-max_count
             while(diff>k):
                 freq[ord(s[low])]-=1
                 #automatically handle frequency when o
                 low+=1
                 max_count = max(freq)
+                #max_count = max(max_count, freq[char_index])
                 current_len=high-low+1
                 diff=current_len-max_count
             current_len=high-low+1
