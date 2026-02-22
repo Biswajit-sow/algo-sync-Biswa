256dca74056e5dd1d7222393f7ad5784f7200c0d
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-02-22 22:05:24 +0530

    Time: 25 ms (47.10%) | Memory: 27.9 MB (49.80%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
@@ -8,11 +8,6 @@ class Solution:
             prev_one_delete=one_delete
             prev_no_delete=no_delete
             no_delete=max(no_delete+arr[i],arr[i])
-            v2=0
-            if prev_one_delete==float('inf'):
-                v2=arr[i]
-            else:
-                v2=prev_one_delete+arr[i]
-            one_delete=max(v2,prev_no_delete)
+            one_delete=max(prev_one_delete+arr[i],prev_no_delete)
             result=max(result,max(no_delete,one_delete))
         return result 
