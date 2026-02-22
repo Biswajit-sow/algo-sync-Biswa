3df1be428541be9eaad86e977e7ef9893e61e49e
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-02-22 22:35:55 +0530

    Time: 20 ms (56.55%) | Memory: 27.9 MB (49.80%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.py
@@ -2,12 +2,18 @@ class Solution:
     def maximumSum(self, arr: List[int]) -> int:
         n=len(arr)
         no_delete=arr[0]
-        one_delete=0
+        one_delete=float('-inf')
         result=arr[0]
         for i in range(1,n):
             prev_one_delete=one_delete
             prev_no_delete=no_delete
-            no_delete=max(no_delete+arr[i],arr[i])
+            no_delete=max(prev_no_delete+arr[i],arr[i])
+            '''v2=0
+            if prev_one_delete==float('inf'):
+                v2=arr[i]
+            else:
+                v2=prev_one_delete+arr[i]
+            one_delete=max(v2,prev_no_delete)'''
             one_delete=max(prev_one_delete+arr[i],prev_no_delete)
             result=max(result,max(no_delete,one_delete))
         return result 
