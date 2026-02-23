c323a772fe458ded45b8e38e902b97e6336dd66a
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-02-23 09:43:24 +0530

    Time: 55 ms (28.44%) | Memory: 31.7 MB (31.47%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -3,10 +3,10 @@ class Solution:
         n=len(nums)
         best_ending_max=nums[0]
         best_ending_min=nums[0]
-        result=nums[0]
+        result=abs(nums[0])
         
         for i in range(1,n):
             best_ending_max=(max(best_ending_max+nums[i],nums[i]))
             best_ending_min=(min(best_ending_min+nums[i],nums[i]))
-            result=max(result,max(abs(best_ending_min),abs(best_ending_max)))
-        return abs(result)
+            result=max(result,max(abs(best_ending_min),(best_ending_max)))
+        return result
