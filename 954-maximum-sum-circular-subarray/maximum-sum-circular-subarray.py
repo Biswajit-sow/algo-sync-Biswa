d9d9d25fc0444bb39dc0a9cfc6c8cdffca790e4a
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-02-24 01:14:49 +0530

    Time: 48 ms (45.76%) | Memory: 24.1 MB (72.32%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -17,8 +17,9 @@ class Solution:
 
             best_min_ending=min(prev_best_min_ending+nums[i],nums[i])
             best_min_ans=min(best_min_ans,best_min_ending)
-        if best_max_ans <=0:
-            return best_max_ans 
+        
         best_max_ending2 = total_sum - best_min_ans
+        if best_max_ending2 ==0:
+            return best_max_ans 
         result = max(best_max_ans, best_max_ending2)
         return result
