aaa43887ce5720ace29ffcdb7b860a3f9af7205c
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-04-01 18:59:38 +0530

    Time: 3 ms (34.29%) | Memory: 19.3 MB (48.38%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -25,3 +25,31 @@ class Solution:
             return False
         
         return True
+
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        matching = {
+                    '(': ')',
+                    '[': ']',
+                    '{': '}'
+                }
+        for i in s :
+            if i in '({[':
+                stack.append(i)
+            elif i in ')}]':
+                if not stack:
+                    return False
+                top_bracket=stack.pop()
+                
+                current=i
+                if matching[top_bracket]==current:
+                    continue
+                else:
+                    return False
+        if  not stack:
+            return True
+        else:
+            return False
+'''
