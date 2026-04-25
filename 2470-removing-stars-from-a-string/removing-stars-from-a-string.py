2c3a8202596ddc5d45f24f6831dd4ceef1f935ab
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-04-25 19:18:52 +0530

    Time: 115 ms (29.42%) | Memory: 20.6 MB (44.77%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -3,19 +3,13 @@ class Solution:
         n=len(s)
         char_stack = []
         res=[]
-        for i in range(n):
-            if len(char_stack) == 0:
-                char_stack.append(s[i])
+       
+        for i in s:
+            if i== "*":
+                if i!= char_stack[-1]:# star when chcek nonstar element
+                    char_stack.pop() 
             else:
-                if s[i]== "*":
-                    if s[i] != char_stack[-1]:# star when chcek nonstar element
-                        char_stack.pop() 
-                else:
-                    char_stack.append(s[i])
-        while len(char_stack)!=0:
-            res.append(char_stack[-1])
-            char_stack.pop()
-        res.reverse()
-        return "".join(res)
+                char_stack.append(i)
+        return ''.join(char_stack)
 
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+        
