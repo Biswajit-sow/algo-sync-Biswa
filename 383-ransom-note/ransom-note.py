aa750890442c19d5139557e1e3ed96becb0750c8
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-05-05 12:09:05 +0530

    Time: 75 ms (5.07%) | Memory: 19.5 MB (94.37%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,18 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        m=len(ransomNote)
-        magazine = list(magazine)   # convert to list so we can remove
-        
-        for i in range(m):
-            found = False
-            for j in range(len(magazine)):
-                if ransomNote[i] == magazine[j]:
-                    magazine.pop(j)   # remove by index
-                    found = True
-                    break
-            if not found:
+        ransomNote = sorted(ransomNote)
+        magazine = sorted(magazine)
+
+        i = 0   # pointer for ransomNote
+        j = 0   # pointer for magazine
+
+        while i < len(ransomNote) and j < len(magazine):
+            if ransomNote[i] == magazine[j]:
+                i += 1
+                j += 1
+            elif ransomNote[i] > magazine[j]:
+                j += 1
+            else:
                 return False
 
-        return True
-                    
+        return i == len(ransomNote)
