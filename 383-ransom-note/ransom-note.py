75e29d61eb6773161429e2e3325911d6dec3063d
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-05-05 12:43:35 +0530

    Time: 30 ms (13.76%) | Memory: 19.7 MB (33.85%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,6 +1,30 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
+        ransom_count = {}
+        magazine_count = {}
+
+        # count frequency of ransomNote
+        for ch in ransomNote:
+            if ch in ransom_count:
+                ransom_count[ch] += 1
+            else:
+                ransom_count[ch] = 1
+
+        # count frequency of magazine
+        for ch in magazine:
+            if ch in magazine_count:
+                magazine_count[ch] += 1
+            else:
+                magazine_count[ch] = 1
+
+        # compare frequencies
+        for ch in ransom_count:
+            if ch not in magazine_count or ransom_count[ch] > magazine_count[ch]:
+                return False
+
+        return True
+        '''
         m=len(ransomNote)
         magazine = list(magazine)   # convert to list so we can remove
         n=len(magazine)
@@ -15,4 +39,25 @@ class Solution:
                 return False
 
         return True
-                    
+
+        
+        ransomNote = sorted(ransomNote)
+        magazine = sorted(magazine)
+
+        i = 0   # pointer for ransomNote
+        j = 0   # pointer for magazine
+
+        while i < len(ransomNote) and j < len(magazine):
+            if ransomNote[i] == magazine[j]:
+                i += 1
+                j += 1
+            elif ransomNote[i] > magazine[j]:
+                j += 1
+            else:
+                i+=1
+                return False
+
+        return i == len(ransomNote)'''
+
+
+
