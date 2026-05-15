277146dcc1e9c1d51983530cf81afb380732757b
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-05-15 12:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.20%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,42 +1,26 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-
-        # frequency of balloon
         balloon_freq = {}
 
         for ch in "balloon":
-            balloon_freq[ch] = balloon_freq.get(ch, 0) + 1
+            balloon_freq[ch] = balloon_freq.get(ch, 0) + 1 
 
-        # frequency of input string
         text_freq = {}
 
         for ch in text:
             text_freq[ch] = text_freq.get(ch, 0) + 1
+        
+        res=float('inf')
+        for  i in balloon_freq:
+           
+            fballon=balloon_freq[i]
+            if i in text_freq:
+                ftext=text_freq[i]
+            else:
+                ftext=0
+            times=ftext//fballon
+            res= int(min(res,times))
+        return res 
 
-        count = 0
 
-        # repeatedly try to form "balloon"
-        while True:
 
-            can_make = True
-
-            # check every character of balloon
-            for ch in balloon_freq:
-
-                # if character not enough
-                if text_freq.get(ch, 0) < balloon_freq[ch]:
-                    can_make = False
-                    break
-
-            # if cannot form balloon anymore
-            if not can_make:
-                break
-
-            # decrease frequencies
-            for ch in balloon_freq:
-                text_freq[ch] -= balloon_freq[ch]
-
-            # one balloon formed
-            count += 1
-
-        return count
