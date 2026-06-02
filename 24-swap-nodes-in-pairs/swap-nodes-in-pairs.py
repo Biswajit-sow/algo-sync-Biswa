b885695ab4061240d2f9a68999ae913486760b72
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-06-02 22:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (22.14%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -3,61 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        def reverse(head, times):
-            curr = head
-            prev = None
-
+        def reverse(head,times):
+            curr=head
+            prev=None
             while curr and times > 0:
-                nxt = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nxt
+                nxt=curr.next
+                curr.next=prev
+                prev=curr
+                curr=nxt
                 times -= 1
-
-            return prev
-
-        if head is None:
+            return 
+        if head==None:
             return head
-
-        res = None
-        prev_left = None
-        size = 2
-
-        while True:
-
-            left = head
-            right = head
-
-            count = 1
+        new_head=None
+        next_left=None
+        prev_left=None
+        size=2
+        while (True):
+            left=head
+            right=head
+            count=1
             while count < size and right:
-                right = right.next
+                right=right.next
                 count += 1
-
             if right:
-                next_left = right.next
-
-                reverse(left, size)
-
+                next_left=right.next
+                reverse(left,size)
                 if prev_left:
-                    prev_left.next = right
-
-                if res is None:
-                    res = right
-
-                prev_left = left
-                head = next_left
-
-            else:
+                    prev_left.next=right
+                if new_head is None:
+                    new_head= right
+                prev_left=left# for next iteration 
+                head=next_left
+            else:# now right null when only one node left 
                 if prev_left:
-                    prev_left.next = left
-
-                if res is None:
-                    res = left
-
+                    prev_left.next=left
+                    # if there is one element then and left = head then till the res is null becuase there is one node so no change in prev ornext left or no right for this 
+                if new_head==None:
+                    new_head=left
                 break
-
-        return res
+        return new_head
