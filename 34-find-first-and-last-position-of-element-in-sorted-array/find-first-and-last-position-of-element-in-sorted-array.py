91770844a62c09a9a8b9dd3bf4ff1a5ef581a067
Biswajit sow <146136784+Biswajit-sow@users.noreply.github.com>
2026-06-11 01:46:34 +0530

    Time: 1 ms (10.92%) | Memory: 20.5 MB (59.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -20,7 +20,7 @@ class Solution:
         def last():
             n=len(nums)
             low=0
-            fast=-1
+            res=-1
             high=n-1
             
             while (low<=high):
@@ -30,7 +30,7 @@ class Solution:
                 elif nums[guess]>target:
                     high=guess-1
                 else:
-                    fast=guess
+                    res=guess
                     low=guess+1
-            return fast
+            return res
         return [first(),last()]
